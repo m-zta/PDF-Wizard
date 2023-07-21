ffc553efb96a7d38adb8d3251bf4cd77d6497f4e
Mario Arana <marioknaus22@gmail.com>
2023-07-21 02:20:25 +0200

    Minor changes

--- pdf_to_text.py
+++ pdf_to_text.py
@@ -2,8 +2,13 @@ import os
 import sys
 import PyPDF2
 
+"""
+Extracts the text from all the PDFs in the input directory and saves
+it to a single output file.
+"""
 
-def extract_text_from_pdf(pdf_path):
+
+def extract_from_pdf(pdf_path):
     with open(pdf_path, 'rb') as file:
         pdf_reader = PyPDF2.PdfReader(file)
         text = ""
@@ -12,13 +17,24 @@ def extract_text_from_pdf(pdf_path):
     return text
 
 
+def transfer_text(input_dir, output_file):
+    with open(output_file, 'w') as output:
+        for filename in os.listdir(input_dir):
+            if filename.endswith(".pdf"):
+                pdf_path = os.path.join(input_dir, filename)
+                text = extract_from_pdf(pdf_path)
+                output.write(text)
+
+    # Print success message
+    print(f"Text extracted from PDFs and saved to {output_file}.")
+
+
 def main():
     # Check if the input directory exists
     if len(sys.argv) != 2:
         print("Usage: python pdf_to_text.py <directory_path>")
         sys.exit(1)
 
-    # Get the input directory path
     input_dir = sys.argv[1]
     output_file = "output.txt"
 
@@ -27,18 +43,10 @@ def main():
         print("Invalid directory path.")
         sys.exit(1)
 
-    # Extract text from PDFs and save to output file
-    with open(output_file, 'w') as output:
-        for filename in os.listdir(input_dir):
-            if filename.endswith(".pdf"):
-                pdf_path = os.path.join(input_dir, filename)
-                text = extract_text_from_pdf(pdf_path)
-                output.write(text)
+    transfer_text(input_dir, output_file)
 
-    # Print success message
     print(f"Text extracted from PDFs and saved to {output_file}.")
 
 
-# Run the main function
 if __name__ == "__main__":
     main()
