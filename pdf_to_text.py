e7af62df662df9138ffbc17a1735075ef4eabafb
Mario Arana <marioknaus22@gmail.com>
2023-07-21 14:21:35 +0200

    Improved CLI

--- pdf_to_text.py
+++ pdf_to_text.py
@@ -1,11 +1,30 @@
 import os
 import sys
 import PyPDF2
+import argparse
 
 """
 Extracts the text from all the PDFs in the input directory and saves
 it to a single output file.
 """
+
+
+def parse_arguments():
+    # The 'description' argument gives a brief description of what the program does when the user passes the -h or --help flags.
+    parser = argparse.ArgumentParser(
+        description='Description: Extracts the text from all the PDFs in the input directory and saves it to a single output file.')
+    parser.add_argument('input_dir', type=str,
+                        help='The directory containing the PDF files.')
+    parser.add_argument('output_file', type=str,
+                        help='The file where the extracted text will be saved.')
+
+    args = parser.parse_args()
+
+    if not os.path.isdir(args.input_dir):
+        print(f"Invalid directory path: {args.input_dir}")
+        sys.exit(1)
+
+    return args.input_dir, args.output_file
 
 
 def extract_from_pdf(pdf_path):
@@ -30,19 +49,7 @@ def transfer_text(input_dir, output_file):
 
 
 def main():
-    # Check if the input directory exists
-    if len(sys.argv) != 2:
-        print("Usage: python pdf_to_text.py <directory_path>")
-        sys.exit(1)
-
-    input_dir = sys.argv[1]
-    output_file = "output.txt"
-
-    # Check if the input directory exists
-    if not os.path.isdir(input_dir):
-        print("Invalid directory path.")
-        sys.exit(1)
-
+    input_dir, output_file = parse_arguments()
     transfer_text(input_dir, output_file)
 
     print(f"Text extracted from PDFs and saved to {output_file}.")
